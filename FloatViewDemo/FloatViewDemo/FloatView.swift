ccccc1b682d59e61e80051079d8cace0adf720a0
wangrui <rui.wang@lrlz.com>
2017-03-23 23:16:16 +0800

    创建FloatView文件

---
+++ FloatViewDemo/FloatViewDemo/FloatView.swift
@@ -0,0 +1,16 @@
+//
+//  FloatView.swift
+//  FloatViewDemo
+//
+//  Created by wangrui on 2017/3/23.
+//  Copyright © 2017年 wangrui. All rights reserved.
+//
+
+import UIKit
+
+class FloatView: UIImageView
+{
+
+    
+
+}
