2a31820ef5961bf07c4f43bc91687a16990e20b4
wangrui <rui.wang@lrlz.com>
2017-03-23 23:21:19 +0800

    创建导航控制器

--- FloatViewDemo/FloatViewDemo/AppDelegate.swift
+++ FloatViewDemo/FloatViewDemo/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
-        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        let vc = ViewController()
+        vc.title = "FloatView_swift"
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
         return true
     }
 }
